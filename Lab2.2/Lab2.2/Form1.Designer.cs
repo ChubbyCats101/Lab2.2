e56ef7c065b2ddb81845346ea201418666bde893
Thanakorn Phiromkul <Thanakorn.ph@kkumail.com>
2023-12-01 22:38:19 +0700

    GPA

---
+++ Lab2.2/Lab2.2/Form1.Designer.cs
@@ -0,0 +1,541 @@
+﻿namespace Lab2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            infoTextBoxSC = new TextBox();
+            infoTextBoxID = new TextBox();
+            infoTextBoxNM = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            SCaverage = new TextBox();
+            label18 = new Label();
+            MinSC = new TextBox();
+            MinID = new TextBox();
+            MinNM = new TextBox();
+            MaxSC = new TextBox();
+            MaxID = new TextBox();
+            MaxNM = new TextBox();
+            label17 = new Label();
+            label16 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            GPAaverage = new TextBox();
+            textBoxF = new TextBox();
+            textBoxD = new TextBox();
+            textBoxDplus = new TextBox();
+            textBoxC = new TextBox();
+            textBoxCplus = new TextBox();
+            textBoxB = new TextBox();
+            textBoxBplus = new TextBox();
+            textBoxA = new TextBox();
+            GPA = new Label();
+            F = new Label();
+            D = new Label();
+            Dplus = new Label();
+            C = new Label();
+            Cplus = new Label();
+            B = new Label();
+            Bplus = new Label();
+            A = new Label();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(infoTextBoxSC);
+            groupBox1.Controls.Add(infoTextBoxID);
+            groupBox1.Controls.Add(infoTextBoxNM);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(264, 242);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "กรอกข้อมูล";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(28, 147);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "ยกเลิก";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(135, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "บันทึก";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // infoTextBoxSC
+            // 
+            infoTextBoxSC.Location = new Point(109, 101);
+            infoTextBoxSC.Name = "infoTextBoxSC";
+            infoTextBoxSC.Size = new Size(100, 23);
+            infoTextBoxSC.TabIndex = 5;
+            // 
+            // infoTextBoxID
+            // 
+            infoTextBoxID.Location = new Point(109, 62);
+            infoTextBoxID.Name = "infoTextBoxID";
+            infoTextBoxID.Size = new Size(149, 23);
+            infoTextBoxID.TabIndex = 4;
+            // 
+            // infoTextBoxNM
+            // 
+            infoTextBoxNM.Location = new Point(109, 29);
+            infoTextBoxNM.Name = "infoTextBoxNM";
+            infoTextBoxNM.Size = new Size(149, 23);
+            infoTextBoxNM.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(13, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 21);
+            label3.TabIndex = 2;
+            label3.Text = "คะแนน";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(3, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 21);
+            label2.TabIndex = 1;
+            label2.Text = "รหัสนักศึกษา";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(13, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 21);
+            label1.TabIndex = 0;
+            label1.Text = "ชื่อ-สกุล";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(SCaverage);
+            groupBox2.Controls.Add(label18);
+            groupBox2.Controls.Add(MinSC);
+            groupBox2.Controls.Add(MinID);
+            groupBox2.Controls.Add(MinNM);
+            groupBox2.Controls.Add(MaxSC);
+            groupBox2.Controls.Add(MaxID);
+            groupBox2.Controls.Add(MaxNM);
+            groupBox2.Controls.Add(label17);
+            groupBox2.Controls.Add(label16);
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(label14);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(GPAaverage);
+            groupBox2.Controls.Add(textBoxF);
+            groupBox2.Controls.Add(textBoxD);
+            groupBox2.Controls.Add(textBoxDplus);
+            groupBox2.Controls.Add(textBoxC);
+            groupBox2.Controls.Add(textBoxCplus);
+            groupBox2.Controls.Add(textBoxB);
+            groupBox2.Controls.Add(textBoxBplus);
+            groupBox2.Controls.Add(textBoxA);
+            groupBox2.Controls.Add(GPA);
+            groupBox2.Controls.Add(F);
+            groupBox2.Controls.Add(D);
+            groupBox2.Controls.Add(Dplus);
+            groupBox2.Controls.Add(C);
+            groupBox2.Controls.Add(Cplus);
+            groupBox2.Controls.Add(B);
+            groupBox2.Controls.Add(Bplus);
+            groupBox2.Controls.Add(A);
+            groupBox2.Location = new Point(296, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(806, 426);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "ผลลัพท์";
+            // 
+            // SCaverage
+            // 
+            SCaverage.Location = new Point(693, 169);
+            SCaverage.Name = "SCaverage";
+            SCaverage.Size = new Size(76, 23);
+            SCaverage.TabIndex = 30;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.Location = new Point(559, 169);
+            label18.Name = "label18";
+            label18.Size = new Size(93, 21);
+            label18.TabIndex = 29;
+            label18.Text = "คะแนนเฉลี่ย";
+            // 
+            // MinSC
+            // 
+            MinSC.Location = new Point(693, 117);
+            MinSC.Name = "MinSC";
+            MinSC.Size = new Size(76, 23);
+            MinSC.TabIndex = 28;
+            // 
+            // MinID
+            // 
+            MinID.Location = new Point(519, 117);
+            MinID.Name = "MinID";
+            MinID.Size = new Size(149, 23);
+            MinID.TabIndex = 26;
+            // 
+            // MinNM
+            // 
+            MinNM.Location = new Point(364, 117);
+            MinNM.Name = "MinNM";
+            MinNM.Size = new Size(149, 23);
+            MinNM.TabIndex = 27;
+            // 
+            // MaxSC
+            // 
+            MaxSC.Location = new Point(693, 70);
+            MaxSC.Name = "MaxSC";
+            MaxSC.Size = new Size(76, 23);
+            MaxSC.TabIndex = 25;
+            // 
+            // MaxID
+            // 
+            MaxID.Location = new Point(519, 70);
+            MaxID.Name = "MaxID";
+            MaxID.Size = new Size(149, 23);
+            MaxID.TabIndex = 8;
+            // 
+            // MaxNM
+            // 
+            MaxNM.Location = new Point(364, 70);
+            MaxNM.Name = "MaxNM";
+            MaxNM.Size = new Size(149, 23);
+            MaxNM.TabIndex = 8;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label17.Location = new Point(264, 119);
+            label17.Name = "label17";
+            label17.Size = new Size(96, 21);
+            label17.TabIndex = 24;
+            label17.Text = "คะแนนต่ำสุด";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label16.Location = new Point(264, 68);
+            label16.Name = "label16";
+            label16.Size = new Size(94, 21);
+            label16.TabIndex = 8;
+            label16.Text = "คะแนนสูงสุด";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.Location = new Point(700, 35);
+            label15.Name = "label15";
+            label15.Size = new Size(57, 21);
+            label15.TabIndex = 8;
+            label15.Text = "คะแนน";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(403, 35);
+            label14.Name = "label14";
+            label14.Size = new Size(67, 21);
+            label14.TabIndex = 8;
+            label14.Text = "ชื่อ-สกุล";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(538, 35);
+            label13.Name = "label13";
+            label13.Size = new Size(100, 21);
+            label13.TabIndex = 8;
+            label13.Text = "รหัสนักศึกษา";
+            // 
+            // GPAaverage
+            // 
+            GPAaverage.Location = new Point(104, 302);
+            GPAaverage.Name = "GPAaverage";
+            GPAaverage.Size = new Size(69, 23);
+            GPAaverage.TabIndex = 23;
+            // 
+            // textBoxF
+            // 
+            textBoxF.Location = new Point(104, 266);
+            textBoxF.Name = "textBoxF";
+            textBoxF.Size = new Size(69, 23);
+            textBoxF.TabIndex = 22;
+            // 
+            // textBoxD
+            // 
+            textBoxD.Location = new Point(104, 233);
+            textBoxD.Name = "textBoxD";
+            textBoxD.Size = new Size(69, 23);
+            textBoxD.TabIndex = 21;
+            // 
+            // textBoxDplus
+            // 
+            textBoxDplus.Location = new Point(104, 200);
+            textBoxDplus.Name = "textBoxDplus";
+            textBoxDplus.Size = new Size(69, 23);
+            textBoxDplus.TabIndex = 20;
+            // 
+            // textBoxC
+            // 
+            textBoxC.Location = new Point(104, 167);
+            textBoxC.Name = "textBoxC";
+            textBoxC.Size = new Size(69, 23);
+            textBoxC.TabIndex = 19;
+            // 
+            // textBoxCplus
+            // 
+            textBoxCplus.Location = new Point(104, 132);
+            textBoxCplus.Name = "textBoxCplus";
+            textBoxCplus.Size = new Size(69, 23);
+            textBoxCplus.TabIndex = 18;
+            // 
+            // textBoxB
+            // 
+            textBoxB.Location = new Point(104, 99);
+            textBoxB.Name = "textBoxB";
+            textBoxB.Size = new Size(69, 23);
+            textBoxB.TabIndex = 17;
+            // 
+            // textBoxBplus
+            // 
+            textBoxBplus.Location = new Point(104, 66);
+            textBoxBplus.Name = "textBoxBplus";
+            textBoxBplus.Size = new Size(69, 23);
+            textBoxBplus.TabIndex = 16;
+            // 
+            // textBoxA
+            // 
+            textBoxA.Location = new Point(104, 33);
+            textBoxA.Name = "textBoxA";
+            textBoxA.Size = new Size(69, 23);
+            textBoxA.TabIndex = 8;
+            // 
+            // GPA
+            // 
+            GPA.AutoSize = true;
+            GPA.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            GPA.Location = new Point(6, 302);
+            GPA.Name = "GPA";
+            GPA.Size = new Size(79, 21);
+            GPA.TabIndex = 15;
+            GPA.Text = "เกรดเฉลี่ย";
+            // 
+            // F
+            // 
+            F.AutoSize = true;
+            F.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            F.Location = new Point(33, 272);
+            F.Name = "F";
+            F.Size = new Size(18, 21);
+            F.TabIndex = 14;
+            F.Text = "F";
+            // 
+            // D
+            // 
+            D.AutoSize = true;
+            D.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            D.Location = new Point(33, 239);
+            D.Name = "D";
+            D.Size = new Size(21, 21);
+            D.TabIndex = 13;
+            D.Text = "D";
+            // 
+            // Dplus
+            // 
+            Dplus.AutoSize = true;
+            Dplus.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Dplus.Location = new Point(33, 202);
+            Dplus.Name = "Dplus";
+            Dplus.Size = new Size(32, 21);
+            Dplus.TabIndex = 12;
+            Dplus.Text = "D+";
+            // 
+            // C
+            // 
+            C.AutoSize = true;
+            C.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            C.Location = new Point(33, 167);
+            C.Name = "C";
+            C.Size = new Size(20, 21);
+            C.TabIndex = 11;
+            C.Text = "C";
+            // 
+            // Cplus
+            // 
+            Cplus.AutoSize = true;
+            Cplus.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Cplus.Location = new Point(33, 134);
+            Cplus.Name = "Cplus";
+            Cplus.Size = new Size(31, 21);
+            Cplus.TabIndex = 10;
+            Cplus.Text = "C+";
+            // 
+            // B
+            // 
+            B.AutoSize = true;
+            B.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            B.Location = new Point(33, 99);
+            B.Name = "B";
+            B.Size = new Size(20, 21);
+            B.TabIndex = 9;
+            B.Text = "B";
+            // 
+            // Bplus
+            // 
+            Bplus.AutoSize = true;
+            Bplus.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Bplus.Location = new Point(33, 64);
+            Bplus.Name = "Bplus";
+            Bplus.Size = new Size(31, 21);
+            Bplus.TabIndex = 8;
+            Bplus.Text = "B+";
+            // 
+            // A
+            // 
+            A.AutoSize = true;
+            A.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            A.Location = new Point(33, 31);
+            A.Name = "A";
+            A.Size = new Size(21, 21);
+            A.TabIndex = 7;
+            A.Text = "A";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.social_credit_removebg_preview1;
+            pictureBox1.Location = new Point(62, 260);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(160, 160);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
+            ClientSize = new Size(1114, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private TextBox infoTextBoxSC;
+        private TextBox infoTextBoxID;
+        private TextBox infoTextBoxNM;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label GPA;
+        private Label F;
+        private Label D;
+        private Label Dplus;
+        private Label C;
+        private Label Cplus;
+        private Label B;
+        private Label Bplus;
+        private Label A;
+        private Button button2;
+        private Label label14;
+        private Label label13;
+        private TextBox GPAaverage;
+        private TextBox textBoxF;
+        private TextBox textBoxD;
+        private TextBox textBoxDplus;
+        private TextBox textBoxC;
+        private TextBox textBoxCplus;
+        private TextBox textBoxB;
+        private TextBox textBoxBplus;
+        private TextBox textBoxA;
+        private TextBox SCaverage;
+        private Label label18;
+        private TextBox MinSC;
+        private TextBox MinID;
+        private TextBox MinNM;
+        private TextBox MaxSC;
+        private TextBox MaxID;
+        private TextBox MaxNM;
+        private Label label17;
+        private Label label16;
+        private Label label15;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Lab2.2/Lab2.2/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System.Diagnostics;
+
+namespace Lab2._2
+{
+    public partial class Form1 : Form
+    {
+        int arrayIndex = 0;
+        int Max = 0;
+        int Min = 0;
+        string[] arrName = new string[10000];
+        string[] arrID = new string[10000];
+        int[] arrScore = new int[10000];
+        int SUM = 0;
+        int na = 0;
+        int nbplus = 0;
+        int nb = 0;
+        int ncplus = 0;
+        int nc = 0;
+        int ndplus = 0;
+        int nd = 0;
+        int nf = 0;
+        string Grade;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputName = infoTextBoxNM.Text;
+            string inputID = infoTextBoxID.Text;
+            int inputScore = int.Parse(infoTextBoxSC.Text);
+
+            arrID[arrayIndex] = inputID;
+            arrName[arrayIndex] = inputName;
+            arrScore[arrayIndex] = inputScore;
+            arrayIndex++;
+
+            infoTextBoxSC.Text = inputScore.ToString();
+
+            if (inputScore > Max)
+            {
+                Max = inputScore;
+                MaxID.Text = inputID;
+                MaxNM.Text = inputName;
+                MaxSC.Text = inputScore.ToString();
+                Min = Max;
+            }
+
+            if (inputScore < Min)
+            {
+                Min = inputScore;
+                MinID.Text = inputID;
+                MinNM.Text = inputName;
+                MinSC.Text = inputScore.ToString();
+            }
+
+            int SUM = Max + Min;
+            double SUM2 = SUM / 2;
+            string SUM3 = SUM2.ToString();
+            SCaverage.Text = SUM3;
+
+            if (inputScore >= 80 && inputScore <= 100)
+            {
+                Grade = "A";
+                na++;
+            }
+            else if (inputScore >= 75 && inputScore < 79)
+            {
+                Grade = "B+";
+                nbplus++;
+            }
+            else if (inputScore >= 70 && inputScore < 74)
+            {
+                Grade = "B";
+                nb++;
+            }
+            else if (inputScore >= 65 && inputScore < 69)
+            {
+                Grade = "C+";
+                ncplus++;
+            }
+            else if (inputScore >= 60 && inputScore < 64)
+            {
+                Grade = "C";
+                nc++;
+            }
+            else if (inputScore >= 55 && inputScore < 59)
+            {
+                Grade = "D+";
+                ndplus++;
+            }
+            else if (inputScore >= 50 && inputScore < 54)
+            {
+                Grade = "D";
+                nd++;
+            }
+            else
+            {
+                Grade = "F";
+                nf++;
+            }
+
+            double v = (na * 4.0) + (nbplus * 3.5) + (nb * 3.0) + (ncplus * 2.5) + (nc * 2.0) + (ndplus * 1.5) + (nd * 1.0);
+            int GPA = (int)v;
+            double GPA2 = GPA / arrayIndex;
+            string GPA3 = GPA2.ToString();
+            GPAaverage.Text = GPA3;
+
+            textBoxA.Text = na.ToString();
+            textBoxBplus.Text = nbplus.ToString();
+            textBoxB.Text = nb.ToString();
+            textBoxCplus.Text = ncplus.ToString();
+            textBoxC.Text = nc.ToString();
+            textBoxDplus.Text = ndplus.ToString();
+            textBoxD.Text = nd.ToString();
+            textBoxF.Text = nf.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            infoTextBoxID.Clear();
+            infoTextBoxNM.Clear();
+            infoTextBoxSC.Clear();
+        }
+    }
+}
